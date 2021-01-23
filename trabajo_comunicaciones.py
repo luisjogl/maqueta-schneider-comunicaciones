dd23be02c3b8c02991a5763f6878c4792283da16
luisjogl <luisjogl@gmail.com>
2021-01-23 15:26:59 +0100

    Variables definidas y esqueleto menu creado

--- trabajo_comunicaciones.py
+++ trabajo_comunicaciones.py
@@ -1,17 +1,7 @@
-#!usr/bin...blablabla
+from opcua import Client
+from opcua import ua
+import time
 
-# Aqui van los imports a todos los programas ficheros necesarios y a las
-# librerias opcua, time y demás que se necesiten
-
-
-
-
-# Aqui va el menu con llamadas a todas las funciones de modos_de_operacion
-def menu():
-
-
-
-# Aqui va definida la funcion main()
 def main():
     # Establece conexion y define variables empleadas
     client = Client("opc.tcp://10.0.0.51:4840")              # construye un objeto cliente OPC UA para un servidor
@@ -21,11 +11,21 @@ def main():
         root = client.get_root_node()                             # navegacion a traves del arbol de objetos
         objects = root.get_child(['0:Objects'])
         m241 = objects.get_child(['2:M241-M251 data'])
-        enable_reset = m241.get_child(['2:POU.IN2'])
-        enable_power = m241.get_child(['2:POU.IN0'])
-        execute_move = m241.get_child(['2:POU.IN1'])
-        velocity_move = m241.get_child(['2:POU.VEL'])
-        execute_stop =
+        enable_reset = m241.get_child(['2:POU.Enable_Reset'])
+        enable_power = m241.get_child(['2:POU.Enable_Power'])
+        execute_moveVel = m241.get_child(['2:POU.Execute_MoveVel'])
+        velocity_moveVel = m241.get_child(['2:POU.VEL'])
+        execute_stop = m241.get_child(['2:POU.Execute_Stop'])
+        set_home = m241.get_child(['2:POU.Set_Home'])
+        home_position = m241.get_child(['2:POU.home_position'])
+        leePosicion = m241.get_child(['2:POU.leePosicion'])
+        leeVelocidad = m241.get_child(['2:POU.leeVelocidad'])
+        execute_moveAbs = m241.get_child(['2:POU.moveAbs'])
+        execute_moveRel = m241.get_child(['2:POU.moveRel'])
+        posicion_objetivo = m241.get_child(['2:POU.posicion_objetivo'])
+        velocidad = m241.get_child(['2:POU.velocidad'])
+        distancia = m241.get_child(['2:POU.distancia'])
+
         menu()
 
     finally:
