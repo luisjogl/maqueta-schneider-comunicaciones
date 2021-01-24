4c0fb6b5a94cacc5515494af75accceddc916a58
luisjogl <luisjogl@gmail.com>
2021-01-25 00:47:29 +0100

    Añadidas funcionalidades y fichero en blanco para testearlas

--- funcionalidades.py
+++ funcionalidades.py
@@ -11,9 +11,10 @@ def encender():
             leeVelocidad.set_value(dv_BOOL(True))
         else:
             pass
-    except error_Power == dv_BOOL(True):
+    except error_Power.get_value() == dv_BOOL(True): #hay que comprobar si es dv_BOOL(False) o directamente False
         execute_reset.set_value(dv_BOOL(True))
         time.sleep(0.5)
+        execute_reset.set_value(dv_BOOL(False))
         enable_power.set_value(dv_BOOL(True))
         leePosicion.set_value(dv_BOOL(True))
         leeVelocidad.set_value(dv_BOOL(True))
@@ -26,22 +27,79 @@ def apagar():
             leeVelocidad.set_value(dv_BOOL(False))
         else:
             pass
-    except error_Power == dv_BOOL(True):
+    except error_Power.get_value() == dv_BOOL(True): #hay que comprobar si es dv_BOOL(False) o directamente False
         execute_reset.set_value(dv_BOOL(True))
         time.sleep(0.5)
+        execute_reset.set_value(dv_BOOL(False))
         enable_power.set_value(dv_BOOL(False))
         leePosicion.set_value(dv_BOOL(False))
         leeVelocidad.set_value(dv_BOOL(False))
 
-#def resetear():
-def set_home():
-
-#opciones menu
-def mover_a(velocidad): #mover cte
-def movimiento_relativo(): #mover relativo desde pos actual o desde pos escogida (set_home)
-def movimiento_absoluto(): #mover hasta pos elegida(posicion_objetivo) a una velocidad
-def parar(): #en cualquier momento
-
 #solo añadir get_value en estas, se usan para base de datos y en cualquiera de los modos de funcionamiento
 def posicion_actual():
+    if leePosicion.get_value() != dv_BOOL(False): #hay que comprobar si es dv_BOOL(False) o directamente False
+        posicion = posicionActual.get_value()
+        print(posicion)
+    else:
+        leePosicion.set_value(dv_BOOL(True))
+        posicion = posicionActual.get_value()
+        print(posicion)
+
 def velocidad_actual():
+    if leeVelocidad.get_value() != dv_BOOL(False): #hay que comprobar si es dv_BOOL(False) o directamente False
+        velocidad = velocidadActual.get_value()
+        print(velocidad)
+    else:
+        leeVelocidad.set_value(dv_BOOL(True))
+        velocidad = velocidadActual.get_value()
+        print(velocidad)
+
+def movimiento_cte(vel): #mover a velocidad constante
+    try:
+        velocity_moveVel.set_value(dv_DINT(vel))
+        execute_moveVel.set_value(dv_BOOL(True))
+
+    except error_MoveVel.get_value() == dv_BOOL(True): #hay que comprobar si es dv_BOOL(False) o directamente False
+        execute_reset.set_value(dv_BOOL(True))
+        time.sleep(0.5)
+        execute_reset.set_value(dv_BOOL(False))
+        velocity_moveVel.set_value(dv_DINT(vel))
+        execute_moveVel.set_value(dv_BOOL(True))
+
+def movimiento_relativo(dist, vel):
+    try:
+        distancia.set_value(dv_DINT(dist))
+        velocidad.set_value(dv_DINT(vel))
+        execute_moveRel.set_value(dv_BOOL(True))
+
+    except error_moveRelativo.get_value() == dv_BOOL(True): #hay que comprobar si es dv_BOOL(False) o directamente False
+        execute_reset.set_value(dv_BOOL(True))
+        time.sleep(0.5)
+        execute_reset.set_value(dv_BOOL(False))
+        distancia.set_value(dv_DINT(dist))
+        velocidad.set_value(dv_DINT(vel))
+        execute_moveRel.set_value(dv_BOOL(True))
+
+def movimiento_absoluto(pos_obj, vel): #mover hasta pos elegida(posicion_objetivo) a una velocidad
+    try:
+        posicion_objetivo.set_value(dv_DINT(pos_obj))
+        velocidad.set_value(dv_DINT(vel))
+        execute_moveAbs.set_value(dv_BOOL(True))
+
+    except error_moveAbsolute.get_value() == dv_BOOL(True): #hay que comprobar si es dv_BOOL(False) o directamente False
+        execute_reset.set_value(dv_BOOL(True))
+        time.sleep(0.5)
+        execute_reset.set_value(dv_BOOL(False))
+        posicion_objetivo.set_value(dv_DINT(pos_obj))
+        velocidad.set_value(dv_DINT(vel))
+        execute_moveAbs.set_value(dv_BOOL(True))
+
+def parar():
+    try:
+        execute_stop.set_value(dv_BOOL(True))
+
+    except error_Stop.get_value() == dv_BOOL(True): #hay que comprobar si es dv_BOOL(False) o directamente False
+        execute_reset.set_value(dv_BOOL(True))
+        time.sleep(0.5)
+        execute_reset.set_value(dv_BOOL(False))
+        execute_stop.set_value(dv_BOOL(True))
