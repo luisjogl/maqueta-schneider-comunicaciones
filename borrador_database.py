6d20ff088621df88afee8ceeee3443cd2e7a36d9
luisjogl <luisjogl@gmail.com>
2021-01-27 15:55:02 +0100

    Borrador database

--- borrador_database.py
+++ borrador_database.py
@@ -23,5 +23,31 @@ def sql_table(con):
     con.commit()
 
 con = sql_connection()
-
+"""
 sql_table(con)
+
+con.execute('''CREATE TABLE COMPANY
+         (ID INT PRIMARY KEY     NOT NULL,
+         NAME           TEXT    NOT NULL,
+         AGE            INT     NOT NULL,
+         ADDRESS        CHAR(50),
+         SALARY         REAL);''')
+print("Table created successfully");"""
+
+con.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+      VALUES (1, 'Paul', 32, 'California', 20000.00 )");
+
+con.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+      VALUES (2, 'Allen', 25, 'Texas', 15000.00 )");
+
+con.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+      VALUES (3, 'Teddy', 23, 'Norway', 20000.00 )");
+
+con.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+      VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 )");
+
+con.commit()
+print("Records created successfully");
+con.close()
+
+con.close()
